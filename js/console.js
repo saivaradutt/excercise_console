aea3c13eb70820909b46d8f2a0ba288b778a5aa7
Sai Varadutt Kumar Suthari <ssuthari@isimtl.local>
2020-02-27 14:46:03 -0500

    first commit

---
+++ js/console.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const temperature = 21.3
+const fruits = ['apple', 'pear', 'orange']
+console.log('YO.')
+console.log(temperature)
+console.log(fruits)
+console.log('The temperature is '+temperature);
+console.log('The collection of fruits is '+fruits)
+console.log('<h1>No html interpreter</h1>')
+console.log(18+ ' eighteen '+ true+ ' null ',undefined)
+console.log('this is a group')
+console.group()
+console.log('group content')
+console.groupEnd()
+console.log('this is second group')
+console.group()
+console.log(' content of second group')
